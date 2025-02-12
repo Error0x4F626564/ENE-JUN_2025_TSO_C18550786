ee13ebdb89fe4fa43b3d945174f48b6dbbf6b5e1
Obed <s_obed27@hotmail.com>
2025-02-12 12:22:51 -0600

    perimer cambio

--- U1/MisDatos.java
+++ U1/MisDatos.java
@@ -2,5 +2,6 @@ public class MisDatos {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
 	System.out.println("Obed");
+	System.out.println("Taller de sistemas Operativos");
     }
 }
